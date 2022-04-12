1d052432a5ef2b5703c225d7a2182b05ea1e32f9
Maycol R <maycol.rodriguez.ma@gmail.com>
2022-04-12 15:29:01 -0500

    configuracion inicial del repositorio

---
+++ main.py
@@ -0,0 +1,9 @@
+for i in range(100):
+    if (i % 15 == 0):
+        print(f'{i} Tingo Tango')
+    elif (i % 3 == 0):
+        print(f'{i} Tingo')
+    elif (i % 5 == 0):
+        print(f'{i} Tango')
+    else:
+        print(i)
---
+++ src/logica/TingoTango.py
@@ -0,0 +1,11 @@
+def TingoTango(numero):
+    if (numero % 15 == 0):
+        return (f'{numero} Tingo Tango')
+    elif (numero % 3 == 0):
+        return (f'{numero} Tingo')
+    elif (numero % 5 == 0):
+        return (f'{numero} Tango')
+    else:
+        return (f'{numero}')
+
+print(TingoTango(5))
